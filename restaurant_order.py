02cfedaac2eeab3d3e99ec058f621127f4f0ad53
mosman7 <mohamedosman998@hotmail.com>
2022-10-12 18:18:47 +0100

    menu

---
+++ restaurant_order.py
@@ -0,0 +1,7 @@
+# User Stories
+
+# As a User I want to be able to see the menu in a formated way, so that I can order my meal.
+menu = ["Chicken curry", "Vegetable curry", "Lamb curry", "Chips", "Chicken Wrap", "Veggie Wrap",
+        "Dr Pepper", "Fanta", "Coke", "Water"]
+for i in menu:
+    print(i)
